ea32648b47adea992c13f47b7a77e1268d75b2fe
Saunved <saunved@gmail.com>
2020-12-23 10:33:25 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <n-link to="/users">Users</n-link>
+    <p v-for="(post, idx) in posts" :key="idx">{{post.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$axios}){
+    if(process.server){
+      const res = await $axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      return {
+        posts: res.data
+      }
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <n-link to="/">Posts</n-link>
+    <p v-for="(user, idx) in users" :key="idx">{{user.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$axios}){
+    if(process.server){
+        const res = await $axios.get(`https://jsonplaceholder.typicode.com/users`)
+        return {
+        users: res.data
+        }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
